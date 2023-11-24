a0ad0ec93ef3ab9efc0203e056db7e25fc56d191
Phanvanbinh2000 <70782594+Phanvanbinh2000@users.noreply.github.com>
2023-11-25 00:47:07 +0700

    oke

---
+++ src/pages/chitietkhachhang/datachitietkhachhang.js
@@ -0,0 +1,99 @@
+export const products = [
+    {
+      id: '1',
+      name: 'Nguyễn Văn A',
+      address: 'Hà Nội',
+      phone: '0123456789',
+      birthYear: '1990',
+      purchaseDate: '2022-01-01'
+    },
+    {
+      id: '2',
+      name: 'Trần Văn B',
+      address: 'Hồ Chí Minh',
+      phone: '0987654321',
+      birthYear: '1991',
+      purchaseDate: '2022-02-01'
+    },
+    {
+      id: '3',
+      name: 'Lê Văn C',
+      address: 'Đà Nẵng',
+      phone: '0123456789',
+      birthYear: '1992',
+      purchaseDate: '2022-03-01'
+    },
+    {
+      id: '4',
+      name: 'Phạm Văn D',
+      address: 'Hải Phòng',
+      phone: '0987654321',
+      birthYear: '1993',
+      purchaseDate: '2022-04-01'
+    },
+    {
+      id: '5',
+      name: 'Nguyễn Văn E',
+      address: 'Cần Thơ',
+      phone: '0123456789',
+      birthYear: '1994',
+      purchaseDate: '2022-05-01'
+    },
+    {
+      id: '6',
+      name: 'Trần Văn F',
+      address: 'Bình Dương',
+      phone: '0987654321',
+      birthYear: '1995',
+      purchaseDate: '2022-06-01'
+    },
+    {
+      id: '7',
+      name: 'Lê Văn G',
+      address: 'Nghệ An',
+      phone: '0123456789',
+      birthYear: '1996',
+      purchaseDate: '2022-07-01'
+    },
+    {
+      id: '8',
+      name: 'Phạm Văn H',
+      address: 'Hà Tĩnh',
+      phone: '0987654321',
+      birthYear: '1997',
+      purchaseDate: '2022-08-01'
+    },
+    {
+      id: '9',
+      name: 'Nguyễn Văn I',
+      address: 'Quảng Bình',
+      phone: '0123456789',
+      birthYear: '1998',
+      purchaseDate: '2022-09-01'
+    },
+    {
+      id: '10',
+      name: 'Trần Văn J',
+      address: 'Quảng Trị',
+      phone: '0987654321',
+      birthYear: '1999',
+      purchaseDate: '2022-10-01'
+    },
+    {
+      id: '11',
+      name: 'Lê Văn K',
+      address: 'Thừa Thiên Huế',
+      phone: '0123456789',
+      birthYear: '2000',
+      purchaseDate: '2022-11-01'
+    },
+    {
+      id: '12',
+      name: 'Phạm Văn L',
+      address: 'Đà Nẵng',
+      phone: '0987654321',
+      birthYear: '2001',
+      purchaseDate: '2022-12-01'
+    },
+    // Thêm dữ liệu mẫu tại đây
+  ];
