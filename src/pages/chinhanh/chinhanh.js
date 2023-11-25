6473927f02d1a8b08c44492a583bc2e6364b4df0
Phanvanbinh2000 <70782594+Phanvanbinh2000@users.noreply.github.com>
2023-11-25 09:41:36 +0700

    chi nhánh

---
+++ src/pages/chinhanh/chinhanh.js
@@ -0,0 +1,111 @@
+export const products = [
+    {
+      id: '1',
+      name: 'Chi Nhánh 1',
+      createdDate: '2022-01-01',
+      dataField: 'id',
+      isKey: true,
+      dataAlign: 'center',
+      dataSort: true
+    },
+    {
+        id: '1',
+        name: 'Chi Nhánh 1',
+        createdDate: '2022-01-01',
+        dataField: 'id',
+        isKey: true,
+        dataAlign: 'center',
+        dataSort: true
+      },
+      {
+        id: '1',
+        name: 'Chi Nhánh 1',
+        createdDate: '2022-01-01',
+        dataField: 'id',
+        isKey: true,
+        dataAlign: 'center',
+        dataSort: true
+      },
+      {
+        id: '1',
+        name: 'Chi Nhánh 1',
+        createdDate: '2022-01-01',
+        dataField: 'id',
+        isKey: true,
+        dataAlign: 'center',
+        dataSort: true
+      },
+      {
+        id: '1',
+        name: 'Chi Nhánh 1',
+        createdDate: '2022-01-01',
+        dataField: 'id',
+        isKey: true,
+        dataAlign: 'center',
+        dataSort: true
+      },
+      {
+        id: '1',
+        name: 'Chi Nhánh 1',
+        createdDate: '2022-01-01',
+        dataField: 'id',
+        isKey: true,
+        dataAlign: 'center',
+        dataSort: true
+      },
+      {
+        id: '1',
+        name: 'Chi Nhánh 1',
+        createdDate: '2022-01-01',
+        dataField: 'id',
+        isKey: true,
+        dataAlign: 'center',
+        dataSort: true
+      },
+      {
+        id: '1',
+        name: 'Chi Nhánh 1',
+        createdDate: '2022-01-01',
+        dataField: 'id',
+        isKey: true,
+        dataAlign: 'center',
+        dataSort: true
+      },
+      {
+        id: '1',
+        name: 'Chi Nhánh 1',
+        createdDate: '2022-01-01',
+        dataField: 'id',
+        isKey: true,
+        dataAlign: 'center',
+        dataSort: true
+      },
+      {
+        id: '1',
+        name: 'Chi Nhánh 1',
+        createdDate: '2022-01-01',
+        dataField: 'id',
+        isKey: true,
+        dataAlign: 'center',
+        dataSort: true
+      },
+      {
+        id: '1',
+        name: 'Chi Nhánh 1',
+        createdDate: '2022-01-01',
+        dataField: 'id',
+        isKey: true,
+        dataAlign: 'center',
+        dataSort: true
+      },
+      {
+        id: '1',
+        name: 'Chi Nhánh 1',
+        createdDate: '2022-01-01',
+        dataField: 'id',
+        isKey: true,
+        dataAlign: 'center',
+        dataSort: true
+      },
+      
+];
--- src/pages/chinhanh/index.jsx
+++ src/pages/chinhanh/index.jsx
@@ -1,11 +1,104 @@
 import React, { useState } from "react";
+import { products } from './chinhanh';
 import "./styles.scss";
 import Modal from "react-bootstrap/Modal";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
 import ReactPaginate from "react-paginate";
+//1
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
+// import BootstrapTable from 'react-bootstrap-table-next';
+
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+
+// Định nghĩa hàm actionFormat
+const actionFormat = (cell, row) => {
+  // let editAction = `<a className="btn btn-secondary btn-sm edit" title="Edit" onClick={this.editData(${row.id})}><i class="fas fa-pencil-alt"></i></a>`; // ví dụ về thao tác sửa
+  // let deleteAction = `<a className="btn btn-danger btn-sm delete" title="Delete" onClick={this.deleteData(${row.id})}><i class="fas fa-trash-alt"></i></a>`; // ví dụ về thao tác xoá
+  // return editAction + " " + deleteAction;
+  
+};
+
+let lastId = products.length > 0 ? parseInt(products[products.length - 1].id) : 0;
+
+const createCustomModal = (onModalClose, onSave, columns, validateState, ignoreEditable) => {
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    // Logic to handle form submission goes here
+    // Tự sinh mã KH
+    lastId++;
+    // Lưu dữ liệu ảo và hiển thị trên lưới dữ liệu
+    const newCustomer = {
+      id: lastId.toString(),
+      name: document.getElementById("tenKhachHang").value,
+      address: document.getElementById("maNhomKhachHang").value,
+      phone: document.getElementById("tenNhomKhachHang").value,
+      birthYear: document.getElementById("soDienThoaiKhachHang").value,
+      purchaseDate: document.getElementById("ngaySinhTu").value
+    };
+    products.push(newCustomer);
+    // Lưu dữ liệu vào localStorage
+    localStorage.setItem('products', JSON.stringify(products));
+    onSave(event);
+  };
+
+  return (
+    <Modal show={true} onHide={onModalClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Thêm Mới Chi Nhánh</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+      <form>
+                        Mã Chi Nhánh:{" "}
+                        <input
+                          type="text"
+                          id="maChiNhanh"
+                          className="form-control"
+                        />
+                        <br />
+                        Tên Chi Nhánh:{" "}
+                        <input
+                          type="text"
+                          id="tenChiNhanh"
+                          className="form-control"
+                        />
+                        <br />
+                        {/*Đơn Giá: <input type="number" id="dongia" className="form-control" /><br />*/}
+                        Ngày Tạo:{" "}
+                        <input
+                          type="date"
+                          id="ngayTao"
+                          className="form-control"
+                        />
+                        <br />
+                        {/*Ghi Chú: <textarea className="form-control" id="elm1" name="area"></textarea><br />*/}
+                        <button type="submit" className="btn btn-success">
+                          Lưu
+                        </button>
+                      </form>
+      </Modal.Body>
+    </Modal>
+  );
+}
+const cellEditProp = {
+  mode: 'dbclick', // Chế độ chỉnh sửa khi nhấp vào ô
+  blurToSave: true, // Lưu khi bỏ focus khỏi ô đang chỉnh sửa
+};
+
+const options = {
+  insertModal: createCustomModal
+};
+
 
 const Chinhanh = () => {
+
+  var selectRowProp = {
+    mode: "checkbox",
+    clickToSelect: true,
+    bgColor: "rgb(238, 193, 213)" 
+  };
+
   const [showModal, setShowModal] = useState(false);
 
   const toggleModal = () => {
@@ -102,11 +195,11 @@ const Chinhanh = () => {
                     </Modal.Body>
                   </Modal>
                   {/* <!--Nút Thêm Mới--> */}
-                  <div>
+                  {/* <div>
                     <Button variant="success" onClick={toggleModal}>
                       Thêm Mới
                     </Button>
-                  </div>
+                  </div> */}
                 </form>
                 {/* <!-- Kết thúc fomt điền điều kiện lọc --> */}
                 {/* <!-- Lưới Dữ Liệu --> */}
@@ -114,98 +207,25 @@ const Chinhanh = () => {
                 <div className="card-body">
                   <div className="row">
                     <div className="table-responsive col-md-2 card-body">
-                      <table
-                        id="example-database datatable-buttons"
-                        className="table table-striped table-bordered dt-responsive nowrap table table-editable table-nowrap align-middle table-edits"
-                        style={{
-                          borderCollapse: "collapse",
-                          borderSpacing: 0,
-                          width: "100%",
-                        }}
+                      <BootstrapTable
+                        data={products}
+                        selectRow={selectRowProp}
+                        striped
+                        hover
+                        condensed
+                        pagination
+                        insertRow={true}
+                        cellEdit={cellEditProp}
+                        deleteRow
+                        search
+                        tableStyle={{ fontFamily: 'Arial, sans-serif', fontSize: '14px' }}
+                        options={options}
                       >
-                        <thead>
-                          <tr>
-                            <th>
-                              <input type="checkbox" id="chonTatCa" />
-                            </th>
-                            <th>STT</th>
-                            <th>Mã Chi Nhánh</th>
-                            <th>Tên Chi Nhánh</th>
-                            <th>Ngày Tạo</th>
-                            <th></th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr data-id="1">
-                            <td>
-                              <input type="checkbox" className="row-checkbox" />
-                            </td>
-                            <td data-field="id" style={{ width: "80px" }}>
-                              1
-                            </td>
-                            <td data-field="name">VD001</td>
-                            <td data-field="age">Nhóm Vàng</td>
-                            <td data-field="id">07/09/2023</td>
-                            <td>
-                              <a
-                                className="btn btn-secondary btn-sm edit"
-                                title="Edit"
-                              >
-                                <i className="fas fa-pencil-alt"></i>
-                              </a>
-                              <a
-                                className="btn btn-danger btn-sm delete"
-                                title="Delete"
-                              >
-                                <i className="fas fa-trash"></i>
-                              </a>
-                            </td>
-                          </tr>
-                          <tr data-id="1">
-                            <td>
-                              <input type="checkbox" className="row-checkbox" />
-                            </td>
-                            <td data-field="id" style={{ width: "80px" }}>
-                              1
-                            </td>
-                            <td data-field="name">VD001</td>
-                            <td data-field="age">Nhóm Vàng</td>
-                            <td data-field="id">07/09/2023</td>
-                            <td>
-                              <a
-                                className="btn btn-secondary btn-sm edit"
-                                title="Edit"
-                              >
-                                <i className="fas fa-pencil-alt"></i>
-                              </a>
-                              <a
-                                className="btn btn-danger btn-sm delete"
-                                title="Delete"
-                              >
-                                <i className="fas fa-trash"></i>
-                              </a>
-                            </td>
-                          </tr>
-                        </tbody>
-                        {/* <!-- end tbody --> */}
-                      </table>
-                      <ReactPaginate
-                        breakLabel="..."
-                        onPageChange={handlePageClick}
-                        pageRangeDisplayed={5}
-                        nextLabel="next >"
-                        pageCount={10}
-                        previousLabel="< previous"
-                        pageClassName="page-item"
-                        pageLinkClassName="page-link"
-                        previousClassName="page-item"
-                        previousLinkClassName="page-link"
-                        nextClassName="page-item"
-                        breakClassName="page-item"
-                        breakLinkClassName="page-link"
-                        containerClassName="pagination"
-                        activeClassName="active"
-                      />
+                        <TableHeaderColumn dataField="id" isKey dataAlign="center" dataSort>Mã Chi Nhánh</TableHeaderColumn>
+                        <TableHeaderColumn dataField="name" dataAlign="center" dataSort>Tên Chi Nhánh</TableHeaderColumn>
+                        <TableHeaderColumn dataField="createdDate" dataAlign="center" dataSort>Ngày Tạo</TableHeaderColumn>
+                      </BootstrapTable>
+                     
                       {/* <!-- Kết Thúc Lưới Dữ Liệu --> */}
                     </div>
                   </div>
