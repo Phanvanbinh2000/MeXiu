76acde0cbb02ca0c6a5647c3bdceb052a35ddfec
Anhanh1704 <anh.duc.mimi@gmail.com>
2023-11-25 09:45:15 +0700

    nm

---
+++ src/pages/donvitinh/datadonvitinh.js
@@ -0,0 +1,33 @@
+export const donvitinh = [
+    {
+      id: '1',
+      tenDonViTinh: 'Thùng',
+      dateCreate: '2022-01-01'
+    },
+    {
+      id: '2',
+      tenDonViTinh: 'Lốc',
+      dateCreate: '2022-02-01'
+    },
+    {
+      id: '3',
+      tenDonViTinh: 'Lon',
+      dateCreate: '2022-03-01'
+    },
+    {
+      id: '4',
+      tenDonViTinh: 'Hộp',
+      dateCreate: '2022-04-01'
+    },
+    {
+      id: '5',
+      tenDonViTinh: 'Vỉ',
+      dateCreate: '2022-05-01'
+    },
+    {
+      id: '6',
+      tenDonViTinh: 'Dây',
+      dateCreate: '2022-06-01'
+    }
+    // Thêm dữ liệu mẫu tại đây
+  ];
--- src/pages/donvitinh/index.jsx
+++ src/pages/donvitinh/index.jsx
@@ -1,70 +1,121 @@
 import React, { useState } from "react";
+import { donvitinh } from './datadonvitinh.js';
 import "./styles.scss";
 import Modal from "react-bootstrap/Modal";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Button from "react-bootstrap/Button";
-import ReactPaginate from "react-paginate";
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.min.css';
+import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
+// import BootstrapTable from 'react-bootstrap-table-next';
+
+import {BootstrapTable, TableHeaderColumn} from 'react-bootstrap-table';
+
+// Định nghĩa hàm actionFormat
+const actionFormat = (cell, row) => {
+  // let editAction = `<a className="btn btn-secondary btn-sm edit" title="Edit" onClick={this.editData(${row.id})}><i class="fas fa-pencil-alt"></i></a>`; // ví dụ về thao tác sửa
+  // let deleteAction = `<a className="btn btn-danger btn-sm delete" title="Delete" onClick={this.deleteData(${row.id})}><i class="fas fa-trash-alt"></i></a>`; // ví dụ về thao tác xoá
+  // return editAction + " " + deleteAction;
+  
+};
+
+
+let lastId = donvitinh.length > 0 ? parseInt(donvitinh[donvitinh.length - 1].id) : 0;
+
+const createCustomModal = (onModalClose, onSave, columns, validateState, ignoreEditable) => {
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    // Logic to handle form submission goes here
+    // Tự sinh mã KH
+    lastId++;
+    // Lưu dữ liệu ảo và hiển thị trên lưới dữ liệu
+    const newCustomer = {
+      id: lastId.toString(),
+      tenDonViTinh: document.getElementById("tenDonViTinh").value,
+      dateCreate: document.getElementById("ngayTao").value
+    };
+    donvitinh.push(newCustomer);
+    // Lưu dữ liệu vào localStorage
+    localStorage.setItem('donvitinh', JSON.stringify(donvitinh));
+    onSave(event);
+  };
+
+  return (
+    <Modal show={true} onHide={onModalClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Thêm Mới Đơn Vị Tính</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <form onSubmit={handleFormSubmit}>
+          Mã Đơn vị Tính: <input type="text" id="maDonViTinh" className="form-control" value={lastId} readOnly /><br />
+          Tên Đơn Vị Tính: <input type="text" id="tenDonViTinh" className="form-control" /><br />
+          <button type="submit" className="btn btn-success">Lưu</button>
+        </form>
+      </Modal.Body>
+    </Modal>
+  );
+}
+const cellEditProp = {
+  mode: 'dbclick', // Chế độ chỉnh sửa khi nhấp vào ô
+  blurToSave: true, // Lưu khi bỏ focus khỏi ô đang chỉnh sửa
+};
+const options = {
+  insertModal: createCustomModal
+};
 
 const Donvitinh = () => {
+  var selectRowProp = {
+    mode: "checkbox",
+    clickToSelect: true,
+    bgColor: "rgb(238, 193, 213)" 
+  };
+
   const [showModal, setShowModal] = useState(false);
+  const [editModal, setEditModal] = useState(false);
 
   const toggleModal = () => {
     setShowModal(!showModal);
   };
-
-  const handlePageClick = () => {};
+  const toggleEditModal = () => {
+    setEditModal(!editModal);
+  };
+  // const handlePageClick = () => {};
 
   return (
     <div className="main-content">
-      <div className="row align-items-center">
-        <div className="page-content">
-          <div className="container-fluid">
-            <div className="col-md-8">
-              <h6 className="page-title">Danh Mục Đơn Vị Tính</h6>
-            </div>
-
+      <div className="page-content">
+        <div className="container-fluid">
+            <h4 className="page-title">Danh Mục Đơn Vị Tính</h4>
             <div className="card">
               <div className="card-body">
-                {/* <!-- Fomt điền điều kiện lọc --> */}
-                <form className="row g-3 needs-validation" noValidate>
+                
+              <div className="row">
                   {/* <!--Mã Sản Phẩm--> */}
                   <div className="col-md-2">
-                    <label htmlFor="maDonViTinh" className="form-label">
-                      Mã Đơn Vị Tính
-                    </label>
-                    <input
-                      type="text"
-                      className="form-control form-control-sm"
-                      id="maDonViTinh"
-                      required
-                    />
+                    <label>Mã Đơn Vị Tính</label>
+                    <input type="text" className="form-control" placeholder="Nhập Mã DVT" id="maDonViTinh" />
                   </div>
                   {/* <!--Kết Thúc Mã Sản Phẩm--> */}
                   {/* <!--Tên Sản Phẩm--> */}
                   <div className="col-md-2">
-                    <label htmlFor="tenDonViTinh" className="form-label">
-                      Tên Đơn Vị Tính
-                    </label>
-                    <input
-                      type="text"
-                      className="form-control form-control-sm"
-                      id="tenDonViTinh"
-                      required
-                    />
+                    <label>Tên Đơn Vị Tính</label>
+                    <input type="text" className="form-control" placeholder="Nhập Tên DVT" id="tenDonViTinh" />
                   </div>
                   {/* <!--Kết Thúc Tên Sản Phẩm--> */}
 
-                  <div>
-                    <button
-                      className="col-md-2 btn btn-primary mb-3"
-                      type="button"
-                      id="buttonTimKiem"
-                    >
-                      <i className="fas fa-search"></i>
-                      {/* <!-- Biểu tượng tìm kiếm Font Awesome --> */}
-                    </button>
+                  <div className="col-md-2">
+                    <button type="submit" className="btn btn-primary mt-4">Tìm Kiếm</button>
                   </div>
+                </div>
+             </div>
+            </div>
+          {/* <!-- Kết thúc khung chứa điều kiện lọc --> */}
 
+
+          {/* <!-- Lưới Dữ Liệu --> */}
+            <div className="row">
+              <div className="col-12">
+                <div className="card">
+                  <div className="card-body">
                   {/* <!--font hiển thị popup--> */}
                   <Modal show={showModal} onHide={toggleModal}>
                     <Modal.Header closeButton>
@@ -72,146 +123,61 @@ const Donvitinh = () => {
                     </Modal.Header>
                     <Modal.Body>
                       <form>
-                        Mã đơn vị tính:{" "}
-                        <input
-                          type="text"
-                          id="maDonViTinh"
-                          className="form-control"
-                        />
-                        <br />
-                        Tên đơn vị tính:{" "}
-                        <input
-                          type="text"
-                          id="tenDonViTinh"
-                          className="form-control"
-                        />
-                        <br />
-                        Ngày Tạo:{" "}
-                        <input type="date" id="ngay" className="form-control" />
-                        <br />
-                        <button type="submit" className="btn btn-success">
-                          Lưu
-                        </button>
+                        Mã Đơn Vị Tính: <input type="text" id="maDonViTinh" className="form-control" /><br />
+                        Tên Đơn Vị Tính: <input type="text" id="tenDonViTinh" className="form-control" /><br />
+                        Ngày Tạo: <input type="text" id="dateCreate" className="form-control" /><br />
+                        <button type="submit" className="btn btn-success">Lưu</button>
                       </form>
                     </Modal.Body>
                   </Modal>
                   {/* <!--Kết Thúc font hiển thị popup--> */}
-                  {/* <!--Nút Thêm Mới--> */}
-                  <div>
-                    <Button variant="success" onClick={toggleModal}>
-                      Thêm Mới
-                    </Button>
-                  </div>
-                </form>
-                {/* <!-- Kết thúc fomt điền điều kiện lọc --> */}
+
+                  {/* <!-- font hiển thị popup sửa --> */}
+                  <Modal show={editModal} onHide={toggleEditModal}>
+                  <Modal.Header closeButton>
+                    <Modal.Title>Sửa Đơn Vị Tính</Modal.Title>
+                  </Modal.Header>
+                  <Modal.Body>
+                    <form>
+                      Mã Đơn Vị Tính: <input type="text" id="maDonViTinh" className="form-control" /><br />
+                      Tên Đơn Vị Tính: <input type="text" id="tenDonViTinh" className="form-control" /><br />
+                      Ngày Tạo: <input type="text" id="dateCreate" className="form-control" /><br />
+                      <button type="submit" className="btn btn-success">Lưu</button>
+                    </form>
+                  </Modal.Body>
+                </Modal>
+                {/* <!-- Kết Thúc font hiển thị popup sửa --> */}
                 {/* <!-- Lưới Dữ Liệu --> */}
 
-                <div className="card-body">
-                  <div className="row">
-                    <div className="table-responsive col-md-2 card-body">
-                      <table
-                        id="example-database datatable-buttons"
-                        className="table table-striped table-bordered dt-responsive nowrap table table-editable table-nowrap align-middle table-edits"
-                        style={{
-                          borderCollapse: "collapse",
-                          borderSpacing: 0,
-                          width: "100%",
-                        }}
-                      >
-                        <thead>
-                          <tr>
-                            <th>
-                              <input type="checkbox" id="select-all" />
-                            </th>
-                            <th>STT</th>
-                            <th>Mã Đơn Vị Tính</th>
-                            <th>Tên Đơn Vị Tính</th>
-                            <th>Ngày Tạo</th>
-                            <th></th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr data-id="1">
-                            <td>
-                              <input type="checkbox" className="row-checkbox" />
-                            </td>
-                            <td data-field="id" style={{ width: "80px" }}>
-                              1
-                            </td>
-                            <td data-field="name">VD001</td>
-                            <td data-field="age">0001</td>
-                            <td data-field="age">Theo Lố</td>
-                            <td data-field="id">07/09/2023</td>
-                            <td>
-                              <a
-                                className="btn btn-secondary btn-sm edit"
-                                title="Edit"
-                              >
-                                <i className="fas fa-pencil-alt"></i>
-                              </a>
-                              <a
-                                className="btn btn-danger btn-sm delete"
-                                title="Delete"
-                              >
-                                <i className="fas fa-trash"></i>
-                              </a>
-                            </td>
-                          </tr>
-                          <tr data-id="1">
-                            <td>
-                              <input type="checkbox" className="row-checkbox" />
-                            </td>
-                            <td data-field="id" style={{ width: "80px" }}>
-                              1
-                            </td>
-                            <td data-field="name">VD001</td>
-                            <td data-field="age">0001</td>
-                            <td data-field="age">Theo Lốc</td>
-                            <td data-field="id">07/09/2023</td>
-                            <td>
-                              <a
-                                className="btn btn-secondary btn-sm edit"
-                                title="Edit"
-                              >
-                                <i className="fas fa-pencil-alt"></i>
-                              </a>
-                              <a
-                                className="btn btn-danger btn-sm delete"
-                                title="Delete"
-                              >
-                                <i className="fas fa-trash"></i>
-                              </a>
-                            </td>
-                          </tr>
-                        </tbody>
-                        {/* <!-- end tbody --> */}
-                      </table>
-                      <ReactPaginate
-                        breakLabel="..."
-                        onPageChange={handlePageClick}
-                        pageRangeDisplayed={5}
-                        nextLabel="next >"
-                        pageCount={10}
-                        previousLabel="< previous"
-                        pageClassName="page-item"
-                        pageLinkClassName="page-link"
-                        previousClassName="page-item"
-                        previousLinkClassName="page-link"
-                        nextClassName="page-item"
-                        breakClassName="page-item"
-                        breakLinkClassName="page-link"
-                        containerClassName="pagination"
-                        activeClassName="active"
-                      />
-                      {/* <!-- Kết Thúc Lưới Dữ Liệu --> */}
-                    </div>
-                  </div>
+
+                <BootstrapTable
+                    data={donvitinh}
+                    selectRow={selectRowProp}
+                    striped
+                    hover
+                    condensed
+                    pagination
+                    insertRow={true}
+                    cellEdit={cellEditProp}
+                    deleteRow
+                    search
+                    tableStyle={{ fontFamily: 'Arial, sans-serif', fontSize: '14px' }}
+                    options={options}
+                  >
+                    <TableHeaderColumn dataField="id" isKey dataAlign="center" dataSort>Mã Đơn vị Tính</TableHeaderColumn>
+                    <TableHeaderColumn dataField="tenDonViTinh" dataAlign="center" dataSort>Tên Đơn vị tính</TableHeaderColumn>
+                    <TableHeaderColumn dataField="dateCreate" dataAlign="center" dataSort>Ngày Tạo</TableHeaderColumn>
+                    
+                  </BootstrapTable>
+
                 </div>
               </div>
             </div>
           </div>
-          {/* <!-- Hết--> */}
+            {/* <!-- end col --> */}
         </div>
+          {/* <!-- end row --> */}
+      
       </div>
     </div>
   );
